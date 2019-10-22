d79a663e33ad2f619e156a3c0e700503d0e94437
julycrystal <erikxins@gmail.com>
2019-10-22 15:16:27 +0530

    Image and audio player added

--- client/src/components/Result/result.js
+++ client/src/components/Result/result.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 import { withRouter } from 'react-router-dom';
 
 class Result extends Component {
@@ -13,6 +14,7 @@ class Result extends Component {
             <div>
                 <table className="ResultTable">
                     <thead>
+                        <th>Image</th>
                         <th>Song</th>
                         <th>Artist</th>
                         <th>Album</th>
@@ -20,9 +22,11 @@ class Result extends Component {
                         <th>Country</th>
                         <th>Currency</th>
                         <th>Release Date</th>
+                        <th>Preview</th>
                     </thead>
                     <tbody>
                         {this.props.response.length === 0 ? <tr>
+                            <td>No Image Available</td>
                             <td>No Data Available</td>
                             <td>No Data Available</td>
                             <td>No Data Available</td>
@@ -30,9 +34,11 @@ class Result extends Component {
                             <td>No Data Available</td>
                             <td>No Data Available</td>
                             <td>No Data Available</td>
+                            <td>No Preview Available</td>
                         </tr> : this.props.response.map((item, index) => {
                             return (
                                 <tr key={index}>
+                                    <td><img src={item._source.artworkUrl100} alt="" /></td>
                                     <td>{item._source.trackName}</td>
                                     <td>{item._source.artistName}</td>
                                     <td>{item._source.collectionName}</td>
@@ -40,6 +46,7 @@ class Result extends Component {
                                     <td>{item._source.country}</td>
                                     <td>{item._source.currency}</td>
                                     <td>{(item._source.releaseDate).substring(0, 10)}</td>
+                                    <td><ReactAudioPlayer src={item._source.previewUrl} controls /></td>
                                 </tr>
                             )
                         })}
